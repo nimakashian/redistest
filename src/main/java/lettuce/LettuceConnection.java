1bc28aa245dde325d64bba0468b986e0f3ae34b3
n.kashian@gmail.com <dreamworld1*>
2020-01-01 13:21:42 +0330

    jedis

--- src/main/java/lettuce/LettuceConnection.java
+++ src/main/java/lettuce/LettuceConnection.java
@@ -2,23 +2,41 @@ package lettuce;
 
 import com.lambdaworks.redis.RedisClient;
 import com.lambdaworks.redis.RedisConnection;
+import com.lambdaworks.redis.RedisFuture;
 import com.lambdaworks.redis.RedisURI;
 import com.lambdaworks.redis.api.StatefulRedisConnection;
+import com.lambdaworks.redis.api.async.RedisAsyncCommands;
 import com.lambdaworks.redis.api.sync.RedisCommands;
 
+import java.util.StringTokenizer;
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Consumer;
+
 public class LettuceConnection {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws  Exception{
         RedisClient redisClient = new RedisClient(
-                RedisURI.create("redis://localhost"));
+                RedisURI.create("redis://localhost/1"));
         StatefulRedisConnection<String, String> connection = redisClient.connect();
 
         System.out.println("Connected to Redis");
 
         RedisCommands<String, String> commands =connection.sync();
-       // commands.set("foo", "bar");
-        String value = commands.get("foo");
+        commands.set("mor0001", "bar0002");
+        String value = commands.get("mor0001");
         System.out.println(value);
-        connection.close();
-        redisClient.shutdown();
+//        connection.close();
+//        redisClient.shutdown();
+
+        System.out.println("-------------------");
+        RedisAsyncCommands commands1=connection.async();
+        RedisFuture future=commands1.get("mor0001");
+//        future.thenAccept(new Consumer<String>() {
+//            @Override
+//            public void accept(String o) {
+//                System.out.println(o);
+//            }
+//        });
+        future.thenAcceptAsync(System.out::println);
+
     }
 }
